126bbafcd380d47e0af122c63411c5bf030bc831
NikkiGujji007 <nikhilgurjar2411@gmail.com>
2022-08-11 18:40:42 +0530

    Update modified_bubble.cpp

--- Sorting/modified_bubble.cpp
+++ Sorting/modified_bubble.cpp
@@ -7,7 +7,7 @@ void modified_bubble_sort(int arr[],int n)
     for(int i=0;i<n-1;i++)
     {
         int flag=0;
-        for(int j=0;j<n-1;j++)
+        for(int j=0;j<n-i-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
